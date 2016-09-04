0f4e45d5461c45ee5de2830ad800b367de911f10
howardchristopher25 <howardchristopher25@gmail.com>
2016-09-03 23:26:49 -0700

    Hello World
    Added the text "Hello World."

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,10 @@
 package hello.world;
-import java.net.*;
-import java.util.Random;
-import java.util.Scanner;
 
 class HelloWorld 
 {
     public static void main(String[] args) 
     {	    
-
+	System.out.println("Hello World!!!");
     }
 }
 
