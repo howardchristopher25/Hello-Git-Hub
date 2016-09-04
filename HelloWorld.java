d7d1333194bb2ea61a62b8c3b5e483ba5994a04a
howardchristopher25 <howardchristopher25@gmail.com>
2016-09-03 23:24:25 -0700

    Hello World
    Hello Git Hub

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package hello.world;
+import java.net.*;
+import java.util.Random;
+import java.util.Scanner;
+
+class HelloWorld 
+{
+    public static void main(String[] args) 
+    {	    
+
+    }
+}
+
